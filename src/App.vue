ec725d3e8d1d33f8d3b556bf307094248e244bbb
Yurii Rybachok <drummer2009@ukr.net>
2024-09-21 20:11:49 +0300

    add dropdown menu for messages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+
+const items = ref([
+  {
+    title: 'Message from A',
+    icon: 'mdi-email',
+    color: 'blue'
+  },
+  {
+    title: 'Message from B',
+    icon: 'mdi-email',
+    color: 'green'
+  },
+  {
+    title: 'Message from C',
+    icon: 'mdi-email',
+    color: 'red'
+  },
+  {
+    title: 'Message from D',
+    icon: 'mdi-email',
+    color: 'yellow'
+  },
+  {
+    title: 'Message from E',
+    icon: 'mdi-email',
+    color: 'purple'
+  },
+  {
+    title: 'Message from F',
+    icon: 'mdi-email',
+    color: 'orange'
+  }
+])
 </script>
 
 <template>
@@ -12,11 +46,23 @@ import { RouterLink, RouterView } from 'vue-router'
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-badge content="6" color="red-darken-2">
-          <v-icon icon="mdi-bell" color="blue-darken-4"></v-icon>
-        </v-badge>
-      </v-btn>
+      <v-menu transition="scale-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-badge content="6" color="red-darken-2">
+              <v-icon icon="mdi-bell" color="blue-darken-4"></v-icon>
+            </v-badge>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in items" :key="index" :value="index">
+            <v-list-item-title>
+              <v-icon :icon="item.icon" :color="item.color"></v-icon
+              >{{ item.title }}</v-list-item-title
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
       <v-btn to="/login" variant="outlined" size="small" color="pink-darken-4" text="Login">
       </v-btn>
     </v-toolbar>
